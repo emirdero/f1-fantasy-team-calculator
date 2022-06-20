cc1a1a59ac7f1595b996a14cab8ddc9643ba4034
Emir Derouiche <emirde@stud.ntnu.no>
2022-06-20 22:17:39 +0200

    move modal, add export

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import React from "react";
-import Modal from "./modal";
 
 export default function Home() {
   const [team, setTeam] = React.useState(
@@ -122,6 +121,41 @@ export default function Home() {
           ))}
         </div>
       </main>
+    </div>
+  );
+}
+
+function Modal(props) {
+  const closeModal = () =>
+    (document.getElementById(props.id).style.display = "none");
+  return (
+    <div id={props.id} className='modal'>
+      <div className='modal-content'>
+        <a onClick={closeModal}>
+          <span className='close'>&times;</span>
+        </a>
+        <div>
+          <div className='grid' style={{ maxWidth: 500000 }}>
+            {props.items.map((constructor) => {
+              return (
+                <a
+                  onClick={() => {
+                    props.setConstructor(constructor);
+                    closeModal();
+                  }}
+                  className='card'
+                >
+                  <p className='pluss'>
+                    {constructor.name}
+                    <br />${constructor.price} -{" "}
+                    {constructor.average_score_current_season.toFixed(2)} points
+                  </p>
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
